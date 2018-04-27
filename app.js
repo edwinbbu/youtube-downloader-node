adbfae60c4d843ad351381b446cb668e00aa3de1
Edwin Babu <edwinbbu@gmail.com>
2018-04-27 15:04:17 +0530

    downloader almost completed

--- app.js
+++ app.js
@@ -1,60 +1,68 @@
-var fs= require('fs');
-var request= require('request');
+var fs = require('fs');
+var request = require('request');
 var cheerio = require('cheerio');
 var path = require('path');
 var youtubedl = require('youtube-dl');
 
-var search = "https://www.youtube.com/results?search_query=keerthichakra+songs";
-var url = "https://www.youtube.com/watch?v=d9RJLn8GndQ";
+var myArgs = process.argv.slice(2);
+var search = '';
+for (var i = 0; i < myArgs.length; i++) {
+    search = search + myArgs[i] + ' ';
+}
+//console.log(search);
+var query = "https://www.youtube.com/results?search_query=" + search;
 
-request(search, function (err, response, body) {
-    if(err)
-    {
+var list = [];
+request(query, function (err, response, body) {
+    if (err) {
         console.log(err);
     }
     //console.log(body)
     var $ = cheerio.load(body);
-    var title=[];
-    //console.log($('a#video-title.yt-simple-endpoint.style-scope.ytd-video-renderer'));
     $('h3.yt-lockup-title').each(function (i, elem) {
-        var a = $(this).find('a').text();
-        //console.log(a.attr('href'));
+        var a = $(this).find('a').attr('href');
+        list.push(a);
     });
-    //console.log(title);
+    var url = "https://www.youtube.com" + list[0];
+    download(url);
 });
 
-var video = youtubedl(url,
-    // Optional arguments passed to youtube-dl.
-    ['--format=18'],
-    // Additional options can be given for calling `child_process.execFile()`.
-    { cwd: __dirname });
 
-// Will be called when the download starts.
-var size = 0;
-video.on('info', function (info) {
-    'use strict';
-    size = parseInt(info.size);
-    size = size/1024;
-    console.log("Video: "+info._filename);
-    console.log("Size: "+size+"KB");
-    var file = path.join(__dirname, info._filename);
-    video.pipe(fs.createWriteStream(file));
+function download(url) {
+    var video = youtubedl(url,
+        // Optional arguments passed to youtube-dl.
+        ['--format=18'],
+        // Additional options can be given for calling `child_process.execFile()`.
+        { cwd: __dirname + "/output" });
 
-});
+    // Will be called when the download starts.
+    var size = 0;
+    video.on('info', function (info) {
+        'use strict';
+        size = parseInt(info.size);
+        var displaysize = size / (1024 * 1024);
+        displaysize=displaysize.toFixed(2);
+        console.log("Video: " + info._filename);
+        console.log("Size: " + displaysize + " MB");
+        var file = path.join(__dirname + "/output", info._filename);
+        video.pipe(fs.createWriteStream(file));
 
-var pos = 0;
-video.on('data', function data(chunk) {
-    'use strict';
-    pos += chunk.length;
+    });
 
-    if (size) {
-        var percent = (pos / size * 100).toFixed(2);
-        process.stdout.cursorTo(0);
-        process.stdout.clearLine(1);
-        process.stdout.write(percent + '%');
-    }
-});
+    var pos = 0;
+    video.on('data', function data(chunk) {
+        'use strict';
+        pos += chunk.length;
 
-video.on('end', function end() {
-    console.log('\nDone');
-});
+        if (size) {
+            var percent = (pos / size * 100).toFixed(2);
+            process.stdout.cursorTo(0);
+            process.stdout.clearLine(1);
+            process.stdout.write(percent + '%');
+        }
+    });
+
+    video.on('end', function end() {
+        console.log('\nDone');
+    });
+};
